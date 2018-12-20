838831ca46da53a7b239435e05475c6556f00ee5
turtle0328 <one19980328@gmail.com>
2018-12-20 15:07:08 +0800

    Project init

---
+++ app/src/main/java/com/example/user/myapplication/Data.java
@@ -0,0 +1,12 @@
+package com.example.user.myapplication;
+
+public class Data {
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
